998c3093b1ef12af1987eb446780fdfed49aaabc
DevNghia <nghiahieumd@gmail.com>
2023-05-07 23:10:57 +0700

    push_code

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -39,8 +39,8 @@ class BannerController extends Controller
         if ($get_image) {
             $get_name_image = $get_image->getClientOriginalName();
             $name_image = current(explode('.', $get_name_image));
-            $new_image = $name_image . '.' . $get_image->getClientOriginalExtension();
-            $get_image->move('/uploads/product', $new_image);
+            $new_image = $name_image . rand(0, 99) . '.' . $get_image->getClientOriginalExtension();
+            $get_image->move('uploads/product', $new_image);
             $banner->banner_image = $new_image;
             $banner->save();
             Session()->put('message', 'Thêm banner thành công');
